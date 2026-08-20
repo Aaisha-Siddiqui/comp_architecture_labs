fee03c0c4ee6da33110a044eb30ca88d44264f29
Aaisha Siddiqui <as11793@st.habib.edu.pk>
2026-08-20 12:01:42 +0500

    lab1 task4A added

---
+++ task4Alab1.s
@@ -0,0 +1,15 @@
+main:
+    li x10, 0x78786464
+    li x11, 0xA8A81919
+    addi x1, x0, 0x100
+    sw x10, 0(x1)
+
+    addi x2, x0, 0x1F0
+    sw x11, 0(x2) 
+
+    lhu x12, 0(x1)
+    lw x13, 0(x2)
+    lw x14, 0(x2)
+
+end:
+    j end
